73564267ca5bf106f1689be96134eb6b87eb2a4c
Terence Jones <memories0615@gmail.com>
2023-09-11 00:55:03 +0000

    good

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request       # import flask framework
+import urllib.request, json
+
+app = Flask(__name__)             # create an app instance
+
+@app.route("/")                   # use the home url
+def hello():                      # method called hello
+   return render_template("index.html") 
+
+@app.route("/<name>")              # route with URL variable /<name>
+def hello_name(name):              # call method hello_name
+    return "Hello "+ TERENCE          # which returns "hello + name  
+
+@app.route("/about")
+def about():
+    name = "Terence"
+    name = request.args.get('name') if request.args.get('name') else "Hello World!" 
+    return render_template("about.html", aboutName=name) 
+
+@app.route("/home")                   # use the home url
+def home():  
+    # We need to add the URL we will be using to fetch information from.
+    # Sometimes this means also sending some data like a key, but not for this one
+    url = "https://xkcd.com/info.0.json"
+    response = urllib.request.urlopen(url)
+    # Once we have the response we need to extract the data we want.
+    data = response.read()
+    dict = json.loads(data) 
+    return render_template("index.html", datum=dict)    # returns "Welcome Home"
+    
+if __name__ == "__main__":        # when running python app.py
+    app.run()                     # run the flask app
